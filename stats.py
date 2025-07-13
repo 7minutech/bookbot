eb980a401f607d85c99f86f9a317bdca437fd471
7minutech <166534037+7minutech@users.noreply.github.com>
2025-07-13 18:09:10 -0500

    feat: get_char_count

--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_word_count(text):
   word_list = text.split()
   return len(word_list)
+
+def get_char_count(text):
+  char_count_dict = {}
+  for char in text:
+    lower_char = char.lower()
+    if lower_char not in char_count_dict:
+      char_count_dict[lower_char] = 1
+    else:
+      char_count_dict[lower_char] += 1
+  return char_count_dict
+
