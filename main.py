377c8f60ffaae0fd0852d1a10bbd5ba29c10ba83
7minutech <166534037+7minutech@users.noreply.github.com>
2025-07-13 18:09:33 -0500

    feat: import get_char_count and print in main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stats import get_word_count
+from stats import get_word_count, get_char_count
 
 def main():
   text = get_book_text("books/frankenstein.txt")
   word_count = get_word_count(text)
+  char_count = get_char_count(text)
   print(f"{word_count} words found in the document")
+  print(char_count)
 
 def get_book_text(path_to_file):
   with open(path_to_file) as f:
