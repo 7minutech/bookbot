e63735e90517061be2b38be84488741955bbfb48
7minutech <166534037+7minutech@users.noreply.github.com>
2025-07-13 17:58:55 -0500

    feat: imported get_word_count from stats

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_word_count
+
 def main():
   text = get_book_text("books/frankenstein.txt")
   word_count = get_word_count(text)
@@ -8,8 +10,4 @@ def get_book_text(path_to_file):
     file_contents = f.read()
     return file_contents
   
-def get_word_count(text):
-  word_list = text.split()
-  return len(word_list)
-
 main()
