a7f00c02aa78be4ef5da23ef8f6fea1e053ae3b2
7minutech <166534037+7minutech@users.noreply.github.com>
2025-07-13 23:34:47 -0500

    feat: get_sorted_chars

--- stats.py
+++ stats.py
@@ -12,3 +12,16 @@ def get_char_count(text):
       char_count_dict[lower_char] += 1
   return char_count_dict
 
+def sort_on(items):
+  return items["num"]
+
+def get_sorted_chars(chars_dict):
+  chars_list_dict = []
+  for k in chars_dict:
+    v = chars_dict[k]
+    tmp_dict = {}
+    tmp_dict["char"] = k
+    tmp_dict["num"] = v
+    chars_list_dict.append(tmp_dict)
+  chars_list_dict.sort(reverse=True, key=sort_on)
+  return chars_list_dict
