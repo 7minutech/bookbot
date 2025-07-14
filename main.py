4fd0cb6b16e4e1864fea6350a8a5d48d34da15e1
7minutech <166534037+7minutech@users.noreply.github.com>
2025-07-13 23:44:56 -0500

    feat: yse sys.argv for path to book with message in main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from stats import get_word_count, get_char_count, get_sorted_chars
+import sys  
 
 def main():
-  book_path = "books/frankenstein.txt"
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  book_path = sys.argv[1]
   text = get_book_text(book_path)
   word_count = get_word_count(text)
   char_count = get_char_count(text)
@@ -29,5 +33,6 @@ def display_title(book_path):
 def display_word_count(word_count):
   print("----------- Word Count ----------")
   print(f"Found {word_count} total words")
+
 
 main()
