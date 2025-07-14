d09ca0cbc9cbc0284bf93eeddfc4fb79eb0eae45
7minutech <166534037+7minutech@users.noreply.github.com>
2025-07-13 23:35:15 -0500

    feat: main displays sorted chars with display functions

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, get_sorted_chars
 
 def main():
-  text = get_book_text("books/frankenstein.txt")
+  book_path = "books/frankenstein.txt"
+  text = get_book_text(book_path)
   word_count = get_word_count(text)
   char_count = get_char_count(text)
-  print(f"{word_count} words found in the document")
-  print(char_count)
+  display_title(book_path)
+  display_word_count(word_count)
+  display_sorted_chars(get_sorted_chars(char_count))
 
 def get_book_text(path_to_file):
   with open(path_to_file) as f:
     file_contents = f.read()
     return file_contents
-  
+
+def display_sorted_chars(sorted_chars):
+  print("--------- Character Count -------")
+  for char_dict in sorted_chars:
+    char = char_dict["char"]
+    num = char_dict["num"]
+    if char.isalpha():
+      print(f"{char}: {num}")
+ 
+def display_title(book_path):
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {book_path}")
+
+def display_word_count(word_count):
+  print("----------- Word Count ----------")
+  print(f"Found {word_count} total words")
+
 main()
